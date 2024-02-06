8f1422493aedbc7e00956e1f2d6dfd8f4041812a
Paya <pjahanbakhsh.new.id@gmail.com>
2024-02-06 16:07:54 +0330

    Traning-02_Finished

--- traning-01/index.js
+++ traning-01/index.js
@@ -1,4 +1,19 @@
-let numOne = +prompt ("enter first number")
-let numTwo = +prompt ("enter second number")
-console.log (numOne+numTwo)
-document.writeln (numOne+numTwo)
+const numOne = +prompt ("Enter First Number")
+const operator = prompt ("Enter Operator")
+const numTwo = +prompt ("Enter Second Number")
+
+if (isNaN(numOne) || isNaN(numTwo)) {
+    console.log("Wrong Number Input!")
+}
+
+if (operator === "+") {
+    console.log(numOne + numTwo)
+} else if (operator === "-") {
+    console.log(numOne - numTwo)
+} else if (operator === "*") {
+    console.log(numOne * numTwo)
+} else if (operator === "/") {
+    console.log(numOne / numTwo)
+} else {
+    console.log("Wrong Operator Input!")
+}
